4b54036771cc5dde6f03b7c57966158c01eb796a
Gregory Rudnick <grudnick@ku.edu>
2018-02-22 15:50:35 -0600

    added ra/dec limits to plots

--- preimage_coord_transform_SpARCS0035.py
+++ preimage_coord_transform_SpARCS0035.py
@@ -20,23 +20,32 @@ import os
 #match new catalog against original catalog - done
 #plot residuals - done
 
+#Modify the geoxytran calls to indicate the column number of the input file
+#figure out whether I should be transforming "forward" or "backward"
+#make x-y plot file for new transformed catalog to see where the
+#poorly transformed galaxies are
+
 #**************
-#make regions file for new transformed catalog
+
 #transform z-band image
-#move generic routines to separte module and keep cluster-specific
+#move generic routines to separate module and keep cluster-specific
 #routines as a file with cluster name
 
-''' 
-Run with
+'''Run with
 import preimage_coord_transform_SpARCS0035 as pct
+
+#this matches the input and reference catalog and runs geomap and
+#geoxytran on these catalogs
 pct.cat_match_SpARCS0035(septol)
-pct.cat_trans(catgogreen, 'SpARCS0035_geomap.db', './geomap_coords.SpARCS0035.in','./SpARCS0035_J1.v0.cat', 'SpARCS0035', 3.0)
 
-
+#this applies the transformation to the whole catalog
+pct.cat_trans_run()
+or
+pct.cat_trans('/Users/grudnick/Work/GOGREEN/Catalogs/Preimaging/SpARCS0035/SPARCS0035_phot_v2.0_USE.fits', 'SpARCS0035_geomap.db', './geomap_coords.SpARCS0035.in','./SpARCS0035_J1.v0.cat', 'SpARCS0035', 3.0)
 
 '''
 
-def cat_match_SpARCS0035(septol):
+def cat_match_SpARCS0035(septol, **kwargs):
 
     '''Written by Gregory Rudnick 10 January 2018
 
@@ -51,19 +60,25 @@ def cat_match_SpARCS0035(septol):
 
     septol: the maximum separation allowed for a match in arcseconds
 
+    OPTIONAL KEYWORD PARAMETERS
+
+    fullcat_trans:  if set ==1 then transform full catalog
+
+
     '''
 
-    #read in the z-band GOGREEN image and the reference data.
+    #read in the z-band GOGREEN catalog and the reference catalog.
     clustname = 'SpARCS0035'
-    (gg_dat, ref_dat, gg_catname) = cat_read_SpARCS0035()
+    (gg_dat, ref_dat, gg_catname, ref_catname) = cat_read_SpARCS0035()
     #print(gg_dat)
 
-    #match catalogs against each other
+    #rename inputs to make it more readable
     raref = np.array(ref_dat['ALPHA_SKY'])
     decref = np.array(ref_dat['DELTA_SKY'])
     rain = gg_dat['RA']
     decin = gg_dat['DEC']
-    
+
+    #match catalogs against each other
     #return matched values
     #catpath = '/Users/grudnick/Work/GOGREEN/Catalogs/Astrometric/SpARCS0035'
     catpath = '.'
@@ -79,7 +94,14 @@ def cat_match_SpARCS0035(septol):
     #run geomap to compute the transformation and geoxytran to
     #transform the input coordinates using that solution.  This also
     #plots the transformed coordinates.
-    georun(clustname, lims)
+    (dbfile, geomap_infile) =  georun(clustname, lims)
+    
+    if 'fullcat_trans' in kwargs.keys():
+        if kwargs['fullcat_trans'] == 1:
+            cat_trans(gg_catname, dbfile, geomap_infile, ref_catname, clustname, septol, \
+                      ramin = lims['ramin'], ramax = lims['ramax'], decmin = lims['decmin'], \
+                      decmax = lims['decmax'])
+
     
 def georun(clustname, lims):
 
@@ -108,7 +130,8 @@ def georun(clustname, lims):
         cmdstr = 'rm ' + geotran_outfile
         os.system(cmdstr)
 
-    iraf.geoxytran(geomap_infile, geotran_outfile, dbfile, geomap_infile, direction="forward")
+    #transform the input coordinates in the geomap input file.  
+    iraf.geoxytran(geomap_infile, geotran_outfile, dbfile, geomap_infile, direction="backward",xcolumn=3,ycolumn=4)
     
     #read in the geomap input catalog that was transformed using geoxytran.
     trans_ggdat = ascii.read(geotran_outfile)
@@ -121,8 +144,13 @@ def georun(clustname, lims):
     posttrans_plotfile = catpath + '/posttrans.' + clustname + '_coordiff.pdf'
     cmt.match_diff_plot(raref,decref,ratrans,dectrans, plotfile = posttrans_plotfile)
 
+    return dbfile, geomap_infile
 
-def cat_trans(incat, dbfile, geomap_infile, refcat, clustname, septol):
+def cat_trans_run():
+    
+    cat_trans('/Users/grudnick/Work/GOGREEN/Catalogs/Preimaging/SpARCS0035/SPARCS0035_phot_v2.0_USE.fits', 'SpARCS0035_geomap.db', './geomap_coords.SpARCS0035.in','./SpARCS0035_J1.v0.cat', 'SpARCS0035', 3.0)
+
+def cat_trans(incat, dbfile, geomap_infile, refcat, clustname, septol, **kwargs):
 
     '''This routine reads in a FITS catalog, writes a temporary output
     ASCII catalog, then transforms this catalog using geoxytran.  It
@@ -144,10 +172,16 @@ def cat_trans(incat, dbfile, geomap_infile, refcat, clustname, septol):
 
     septol: the maximum separation allowed for a match in arcseconds
 
+    OPTIONAL KEYWORDS
+
+    ramin, ramax, decmin, decmax.  These are the limits over which the
+    transform was originally computed.  If these are given then it
+    uses those limits to color the points in the ra and decdiff plots.
+    If one is given, all must be given.
 
     '''
 
-    #read in photometry catalog
+    #read in GMOS photometry catalog
     cat_hdul = fits.open(incat)
     cat_dat = cat_hdul[1].data
 
@@ -173,19 +207,17 @@ def cat_trans(incat, dbfile, geomap_infile, refcat, clustname, septol):
     fo = open(tmpin, "w")
     fo.write("# ra dec\n")
     for i,val in enumerate(cat_dat['RA']):
-        #print(i,iclose[i])
-        #print(raref[iclose[i]],decref[iclose[i]],rain[idx[iclose[i]]],decin[idx[iclose[i]]])
         fo.write('{} {}\n'.format(cat_dat['RA'][i],cat_dat['DEC'][i]))
     fo.close()
 
-    iraf.geoxytran(tmpin, tmpout, dbfile, geomap_infile, direction="forward",\
+    iraf.geoxytran(tmpin, tmpout, dbfile, geomap_infile, direction="backward",\
                    xcolumn=1, ycolumn = 2)
 
     #read in ascii output file
     trans_dat = ascii.read(tmpout)
     ratrans = np.array(trans_dat['ra'])
     dectrans = np.array(trans_dat['dec'])
-
+    
     #replace the old RAs and DECs with new RAs and DECs in place    
     tcat_hdul = cat_hdul
     tcat_hdul[1].data['RA'] = ratrans
@@ -195,11 +227,22 @@ def cat_trans(incat, dbfile, geomap_infile, refcat, clustname, septol):
     tcat_hdul.writeto(newcat)
 
     #match new catalog against original catalog
-    (rarefm,decrefm,ratransm,dectransm, translims) = cmt.cat_sky_match(raref, decref, ratrans, dectrans, septol)
+    mfile = "allcat_match.txt"
+    (rarefm,decrefm,ratransm,dectransm, translims) = cmt.cat_sky_match(raref, decref, \
+                                                                       ratrans, dectrans, septol, \
+                                                                       matchfile = mfile)
 
     #make a plot of the residuals
     allcattrans_plotfile = 'allcat_trans.' + clustname + '_coordiff.pdf'
-    cmt.match_diff_plot(rarefm,decrefm,ratransm,dectransm, plotfile = allcattrans_plotfile)
+    #passes ra and dec limits if they are defined to find source
+    #outside of ra and dec lims.  Assumes that if one keyword is given
+    #that all are given
+    if 'ramin' in kwargs.keys():
+        cmt.match_diff_plot(rarefm,decrefm,ratransm,dectransm, plotfile = allcattrans_plotfile, \
+                            ramin = kwargs['ramin'], ramax = kwargs['ramax'], \
+                            decmin = kwargs['decmin'], decmax = kwargs['decmax'])
+    else:
+        cmt.match_diff_plot(rarefm,decrefm,ratransm,dectransm, plotfile = allcattrans_plotfile)
 
     
 def cat_read_SpARCS0035():
@@ -220,6 +263,6 @@ def cat_read_SpARCS0035():
     ref_dat = ascii.read(refcat)
 
 
-    return gg_dat, ref_dat, catgogreen;
+    return gg_dat, ref_dat, catgogreen, refcat;
 
     
